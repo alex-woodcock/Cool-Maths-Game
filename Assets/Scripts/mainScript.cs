db394a395c145dea9adeed056a084b328b306801
alex-woodcock <alexanderpwoodcock@gmail.com>
2019-01-28 16:20:03 +0000

    it begins

---
+++ Assets/Scripts/mainScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainScript : MonoBehaviour {
+    public GameObject Shadow;
+	// Use this for initialization
+	void Start () {
+        for (int i=1; i<5; i++)
+        {
+            GameObject shadowClone = Instantiate(Shadow, new Vector2((i-2)*3+0.5f, -1), Quaternion.Euler(new Vector3(0,0,0)));
+            shadowClone.gameObject.name = "shadow"+i.ToString();
+            shadowClone.SendMessage("OnInstantiation", i);
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Question()
+    {
+        for (int i=1; i<5; i++)
+        {
+            Shadow.SendMessage("OnQuestion", 42);
+        }
+    }
+
+    public static void Clicked(int type, int answer)
+    {
+
+    }
+}
---
+++ Assets/Scripts/shadowScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shadowScript : MonoBehaviour {
+    private int type = 0;
+    private int answer = 0;
+    bool mouseOver = false;
+    //1 = blue
+    //2 = green
+    //3 = red
+    //4 = yellow
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void OnInstantiation(int typeData)
+    {
+        type = typeData;
+    }
+
+    void OnQuestion(int answerData)
+    {
+        answer = answerData;
+    }
+
+    private void OnMouseOver()
+    {
+        mouseOver = true;
+    }
+
+    private void OnMouseExit()
+    {
+        mouseOver = false;
+    }
+
+    private void OnMouseDown()
+    {
+        if (mouseOver)
+        {
+            mainScript.Clicked(type, answer);
+        }
+    }
+}
