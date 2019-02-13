f21e27a1cf8dfc57d4d60cd122d37eb794db5b6f
alex-woodcock <alexanderpwoodcock@gmail.com>
2019-02-13 10:53:25 +0000

    ium not sure what this chaneg is but its prolly important

--- Assets/Scripts/textBoxScript.cs
+++ Assets/Scripts/textBoxScript.cs
@@ -6,16 +6,16 @@ using System.IO;
 
 public class textBoxScript : MonoBehaviour {
     public string text;
-    string path = "Assets/Resources/SaveFile.txt";
-    string[] lines;
+    //string path = "Assets/Resources/SaveFile.txt";
+    //string[] lines;
     // Use this for initialization
     void Start () {
         GetComponent<TextMeshProUGUI>().text = "";
-        lines = File.ReadAllLines(path);
+        //lines = File.ReadAllLines(path);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        GetComponent<TextMeshProUGUI>().text = "YOU SCORED: " + lines[0] + "\n\nTOP SCORE: " + lines[1];
+        GetComponent<TextMeshProUGUI>().text = text;
     }
 }
--- Assets/fileReaderForVicTextBox.cs
+++ Assets/fileReaderForVicTextBox.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using System.IO;
 
 public class fileReaderForVicTextBox : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+    string path = "Assets/Resources/SaveFile.txt";
+    string[] lines;
+    // Use this for initialization
+    void Start () {
+        GetComponent<TextMeshProUGUI>().text = "";
+        lines = File.ReadAllLines(path);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        GetComponent<TextMeshProUGUI>().text = "YOU SCORED: " + lines[0] + "\n\nTOP SCORE: " + lines[1];
+    }
 }
